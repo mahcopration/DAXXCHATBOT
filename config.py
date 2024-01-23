bc27516e78d6acd068a87db5f363c9168865602c
mahcopration <129929195+mahcopration@users.noreply.github.com>
2024-01-24 04:03:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "10917377"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "22bfe49f4482c5cb5424729249a5f097"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6112399514"
+SUPPORT_GRP = "Tamil_HDLatest_Movies"
+UPDATE_CHNL = "Call_me_futurepilot"
+OWNER_USERNAME = "Mahsoommjm"
 
